dee3b2dc7f3bca0e27a1d5dcd76cbe13914f48bb
Markus Haverinen <markushave@gmail.com>
2021-01-24 17:18:56 +0200

    add data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useStyles } from './styles';
+import axios from 'axios';
+import { Game } from './interfaces/game.interface';
+import { Container, Typography, Box } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [gamesData, setGamesData] = useState<Array<Game> | undefined>(
+    undefined
   );
-}
+  const styles = useStyles();
+  const BASE_URL = 'https://api.rawg.io/api';
+
+  const getData = async (query: string): Promise<void> => {
+    axios
+      .get(`${BASE_URL}${query}`)
+      .then((res) => {
+        const gamesList: Array<Game> = res.data.results.map((game: any) => {
+          return {
+            id: game.id,
+            name: game.name,
+            platforms: game.platforms,
+            background_image: game.background_image,
+            releaseDate: game.released,
+            metacritic: game.metacritic,
+          };
+        });
+
+        setGamesData(gamesList);
+        console.log(gamesData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getData('/games');
+  }, []);
+
+  return (
+    <Container className={styles.root} maxWidth='sm'>
+      <Box>
+        <Typography variant='h2'>GameApp</Typography>
+      </Box>
+      <Typography>Test</Typography>
+    </Container>
+  );
+};
 
 export default App;
