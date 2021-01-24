44846dfbe3b72b0238f2ed832721d1510006f810
Markus Haverinen <markushave@gmail.com>
2021-01-24 17:18:38 +0200

    add gamecard component

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Card, Typography } from '@material-ui/core';
+import { Game } from '../interfaces/game.interface';
+
+export const GameCard = (game: Game) => {
+  return <Card></Card>;
+};
