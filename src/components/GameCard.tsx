69ee8680ae08d18eb3f4512c4fd2656f96daf8e6
Markus Haverinen <markushave@gmail.com>
2021-01-24 18:20:57 +0200

    add contents

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
-import { Card, Typography } from '@material-ui/core';
+import {
+  Card,
+  Box,
+  CardHeader,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@material-ui/core';
 import { Game } from '../interfaces/game.interface';
+import { useStyles } from '../styles';
 
-export const GameCard = (game: Game) => {
-  return <Card></Card>;
+type GameCardProps = {
+  game: Game;
+};
+
+export const GameCard = ({ game }: GameCardProps) => {
+  const styles = useStyles();
+  return (
+    <Card className={styles.card}>
+      <CardHeader title={game.name} />
+      <CardContent>
+        <CardMedia className={styles.media} image={game.background_image} />
+        <Typography>Released: {game.releaseDate}</Typography>
+        <Typography>Metacritic score: {game.metacritic}</Typography>
+      </CardContent>
+    </Card>
+  );
 };
