503f11757da81e5ec63f0bde98389c5d45e9d128
Markus Haverinen <markushave@gmail.com>
2021-01-30 19:02:54 +0200

    added sorting and pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import { useStyles } from './styles';
 import axios from 'axios';
 import { Game } from './interfaces/game.interface';
 import { GameCard } from './components/GameCard';
-import { Container, Typography, Box } from '@material-ui/core';
+import { PaginationBox } from './components/PaginationBox';
+import { Container, Typography, Box, Grid, Button } from '@material-ui/core';
 
 const App = () => {
+  const [page, setPage] = useState<number>(1);
+  const [query, setQuery] = useState<string>('');
   const [gamesData, setGamesData] = useState<Array<Game> | undefined>(
     undefined
   );
   const styles = useStyles();
-  const BASE_URL = 'https://api.rawg.io/api';
+  const BASE_URL = 'https://api.rawg.io/api/games';
 
-  const getData = async (query: string): Promise<void> => {
+  const getData = async (_page = page, _query = query): Promise<void> => {
+    setPage(_page);
+    setQuery(_query);
+    setGamesData(undefined);
+
     axios
-      .get(`${BASE_URL}${query}`)
+      .get(`${BASE_URL}?page=${_page}${_query}`)
       .then((res) => {
         const gamesList: Array<Game> = res.data.results.map((game: any) => {
           return {
@@ -28,7 +35,6 @@ const App = () => {
         });
 
         setGamesData(gamesList);
-        console.log(gamesData);
       })
       .catch((err) => {
         console.log(err);
@@ -36,7 +42,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    getData('/games');
+    getData();
   }, []);
 
   return (
@@ -44,6 +50,42 @@ const App = () => {
       <Box>
         <Typography variant='h2'>GameApp</Typography>
       </Box>
+      <Box>
+        <Typography variant='h6'>Sort by:</Typography>
+        <Box>
+          <Grid
+            container
+            direction='row'
+            justify='space-evenly'
+            alignItems='center'>
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={() => getData(page, '&ordering=name')}>
+              Name
+            </Button>
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={() => getData(page, '&ordering=-metacritic')}>
+              Metascore
+            </Button>
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={() => getData(page, '&ordering=-released')}>
+              Release date
+            </Button>
+            <Button
+              variant='contained'
+              color='secondary'
+              onClick={() => getData(1, '')}>
+              Reset
+            </Button>
+          </Grid>
+        </Box>
+      </Box>
+      <PaginationBox getData={getData} page={page} />
       <Box>
         {gamesData ? (
           gamesData.map((game) => {
